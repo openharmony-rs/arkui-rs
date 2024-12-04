64faea3ce42672003fbd92ec0d39611547f90edd
Jonathan Schwender <schwenderjonathan@gmail.com>
2024-12-04 14:32:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## ArkUI native bindings for OpenHarmony
+
+Safe Rust bindings to [ArkUI Native](https://docs.openharmony.cn/pages/v5.0/zh-cn/application-dev/reference/apis-arkui/_ark_u_i___native_module.md)
+APIs on OpenHarmony. 
+
+## Development
+
+This crate is still under development and contributions are welcome.
+
+## License
+
+This crate is licensed under the Apache-2.0 license, matching the OpenHarmony OS SDK.
---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+//! ArkUI Native
+//!
+//! Safe Rust abstractions to interact with ArkUI from native Rust code.
+//! This crate is still under development and only provides safe abstractions for a subset
+//! of the available C APIs. Currently, that is:
+//!
+//! - APIs of ArkUI to register gesture callbacks.
+//!
+//! The following features will hopefully be added soon:
+//!
+//! - drag and drop APIs of ArkUI
+//! - animation callbacks of ArkUI
+//! - UI capabilities such as UI component creation and destruction, tree node operations,
+//!   attribute setting, and event listening.
+//!
+//! See the official upstream documentation on [ArkUI Native] for more information.
+//!
+//! [ArkUI Native]: https://docs.openharmony.cn/pages/v5.0/zh-cn/application-dev/reference/apis-arkui/_ark_u_i___native_module.md
+//!
+pub mod gestures;
